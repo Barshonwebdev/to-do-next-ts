c4274ebbdd75c296b8327728725a13c125c6856e
Barshon <barshonweb@gmail.com>
2025-01-26 17:20:16 +0600

    frontend crud todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Todo() {
   
   return (
     <div className="bg-white" >
-      <h1 className="text-center text-5xl mt-5">To Do App</h1>
+      <h1 className= " text-center text-5xl mt-5">To Do App</h1>
       <div className="flex justify-center mt-8 space-x-2">
         <input  onChange={(e)=> updateInput(e.target.value)} value={userInput} className="p-1 border border-gray border-2" type="text" />
         <button onClick={handleAddorEdit} className="bg-blue-500 text-white px-2 py-1 hover:bg-green-500">{editIndex !== null ? 'Update task' : 'Add task'}</button>
@@ -54,7 +54,7 @@ export default function Todo() {
           {
             list.map((item,index)=>(
             <div className="space-x-2" key={item.id}>
-              <span className="text-green-500 text-2xl">{item.value}</span>
+              <span className=" text-green-500 text-2xl">{item.value}</span>
               <span><button  onClick={() => deleteItem(item.id)} className="p-1 text-white bg-red-500 rounded">Delete</button></span>
               <span><button  onClick={() => startEdit(index)} className="p-1 text-white bg-indigo-500 rounded">Edit</button></span>
             </div>
