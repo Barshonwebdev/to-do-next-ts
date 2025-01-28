a6e211014da5c8f0ddc6a786782ad71655376a89
Barshon <barshonweb@gmail.com>
2025-01-28 12:42:34 +0600

    crud complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-
-
+import { RxCross2 } from "react-icons/rx";
+import { CiEdit } from "react-icons/ci";
 export default  function Todo() {
   const [userInput,setUserInput]=useState('');
   const [list,setList]=useState([]);
@@ -32,12 +32,14 @@ export default  function Todo() {
     setList(data);
   }
 
-  async function addTask(list,item){
+  async function addTask(item){
     await fetch('/api',{
       method:"POST",
       body:JSON.stringify(item)
     })
-    setList([...list, item]);
+    const res= await fetch('http://localhost:3000/api');
+    const data= await res.json();
+    setList(data);
 
   }
   //add task or edit task
@@ -58,7 +60,7 @@ export default  function Todo() {
          
           value: userInput,
       };
-      addTask(list,newItem);
+      addTask(newItem);
       
   } 
   
@@ -84,22 +86,23 @@ export default  function Todo() {
   return (
     <div className="bg-gray-100 flex flex-col justify-center py-8 w-1/2 mx-auto" >
       <h1 className= " text-center text-5xl mt-5">To Do App</h1>
-      <div className="  flex flex-col md:flex-row space-y-3 justify-center mt-8 space-x-2">
-        <input  onChange={(e)=> updateInput(e.target.value)} value={userInput} className="sm:mx-auto md:mx-0  border-gray border-2" type="text" />
-        <button onClick={()=>handleAddorEdit(editItem?._id)} className="bg-blue-500 text-white px-2 py-1 hover:bg-green-500">{editItem !== null ? 'Update task' : 'Add task'}</button>
+      <div className="  flex mx-auto space-y-3 justify-center mt-8 space-x-2 ">
+        <input  onChange={(e)=> updateInput(e.target.value)} value={userInput} className="sm:mx-auto md:mx-0  border-gray border-2 px-4" type="text" />
+        <button onClick={()=>handleAddorEdit(editItem?._id)} className="bg-blue-500 rounded-lg text-white px-4 py-1 hover:bg-green-500">{editItem !== null ? 'Update task' : 'Add task'}</button>
         
       </div>
       <div className="  mt-5 ">
-         <div className=" flex flex-col justify-center items-center"  >
+        <h1 className="text-center text-gray-400 mb-3 text-lg">To Do</h1>
+         <div className=" flex flex-col justify-center items-center space-y-3"  >
          {
-            list.map((item,index)=>(
-            <div className=" flex flex-col " key={index}>
+            list.map((item)=>(
+            <div className=" flex flex-col space-y-2 " key={item._id}>
               <div className="text-center">
             <span className=" text-green-500 font-bold text-2xl">{item.value}</span>
               </div>
               <div className="space-x-2 flex justify-center items-center">
-              <span><button  onClick={() => deleteTask(item._id)} className="p-1 text-white bg-red-500 rounded">Delete</button></span>
-              <span><button  onClick={() => startEdit(item)} className="p-1 text-white bg-orange-500 rounded">Edit</button></span>
+              <span className=" "><button  onClick={() => deleteTask(item._id)} className="flex items-center p-1 text-white bg-red-500 rounded "><RxCross2></RxCross2>Delete</button></span>
+              <span><button  onClick={() => startEdit(item)} className="flex items-center p-1 text-white bg-orange-500 rounded"><CiEdit></CiEdit>Edit</button></span>
               </div>
             </div>
             ))
