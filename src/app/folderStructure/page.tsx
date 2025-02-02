5a7b118dc41ae3399327064397b018650d42734d
Barshon <barshonweb@gmail.com>
2025-02-02 17:36:28 +0600

    folder structure viewer model and schema

--- src/app/folderStructure/page.tsx
+++ src/app/folderStructure/page.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { useState } from "react";
+
 export default function folderStructure() {
-  return <div><h1 className="text-center text-3xl text-indigo-500 mt-4">Folder Structure Viewer</h1></div>;
+    const [children,setChildren]=useState([]);
+    const [clicked,setClicked]=useState(false);
+    const [folderName,setFolderName]=useState('');
+
+
+  return <div>
+    <h1 className="text-center text-3xl text-indigo-500 my-4">Folder Structure Viewer</h1>
+        <div className="bg-gray-100 mx-96 p-5 rounded-xl">
+            <p>root</p>
+            <div className="mx-10 my-3">{children.map(children=>(
+                <div>
+                    {children} 
+                </div>)
+            )}</div>
+        </div>
+    </div>;
 }
---
+++ src/models/Folders.ts
@@ -0,0 +1,16 @@
+import mongoose, { Model } from "mongoose";
+type TFolderModel={
+    name:string,
+    path:string,
+    children:TFolderModel
+};
+
+const folderSchema= new mongoose.Schema<TFolderModel>({
+    name:{type:String,required:true},
+    path:{type:String,required:true},
+    children:{type:{},required:true}
+});
+
+const FolderModel:Model<TFolderModel>=mongoose.models.FolderModel || mongoose.model('FolderModel', folderSchema);
+
+export default FolderModel;
